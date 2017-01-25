0986abeaaa77527f82bacbd077988f195dd50b90
jiangyuanyuan <jiangyuanyuan@1b2b.cn>
2017-01-25 11:31:52 +0800

    modify

--- output/statics/modules/common/global_4491a7e.js
+++
@@ -1,9 +0,0 @@
-define('common/global', function(require, exports, module) {
-
-$(document).ready(function(){
-    $(document).click(function(){
-        $(".conten_right").addClass('open')
-    })
-})
-
-});
